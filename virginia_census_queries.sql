72ce5475deffbf15c202fcf2cb8a9f6ccd9f67c2
Shawn Bryant <sbrya025@odu.edu>
2025-11-18 10:28:38 -0500

    Add more queries

--- virginia_census_queries.sql
+++ virginia_census_queries.sql
@@ -18,9 +18,24 @@ select county, totalpop, hispanic, white, black, native, asian, pacific,
 	ROUND(CAST(pacific AS NUMERIC(10, 2)) / totalpop * 100, 2) AS pacific_percent
 from virginia order by county
 
-
 -- What is the type of work breakdown by county?
-select, county, public_work, private_work, self_employed, family_work
+select county, public_work, private_work, self_employed, family_work, unemployment,
+	ROUND(CAST(public_work AS NUMERIC(10, 2)) / totalpop * 100, 2) AS puw_percent,
+	ROUND(CAST(private_work AS NUMERIC(10, 2)) / totalpop * 100, 2) AS prw_percent,
+	ROUND(CAST(self_employed AS NUMERIC(10, 2)) / totalpop * 100, 2) AS sew_percent,
+	ROUND(CAST(family_work AS NUMERIC(10, 2)) / totalpop * 100, 2) AS fw_percent,
+	ROUND(CAST(unemployment AS NUMERIC(10, 2)) / totalpop * 100, 2) AS u_percent
+from virginia order by county
+
+-- What is the transportation breakdown in population by county?
+select county, totalpop, drive, carpool, transit, walk, other_transp, work_at_home,
+	ROUND(CAST(drive AS NUMERIC(10, 2)) / totalpop * 100, 2) AS drive_percent,
+	ROUND(CAST(carpool AS NUMERIC(10, 2)) / totalpop * 100, 2) AS carpool_percent,
+	ROUND(CAST(transit AS NUMERIC(10, 2)) / totalpop * 100, 2) AS transit_percent,
+	ROUND(CAST(walk AS NUMERIC(10, 2)) / totalpop * 100, 2) AS walk_percent,
+	ROUND(CAST(other_transp AS NUMERIC(10, 2)) / totalpop * 100, 2) AS other_transp_percent,
+	ROUND(CAST(work_at_home AS NUMERIC(10, 2)) / totalpop * 100, 2) AS work_at_home_percent
+from virginia order by county
 
 -- What counties have the highest highest unemployment rate?
 select county, unemployment,  from virginia order by unemployment
